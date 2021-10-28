4b541bea8384c4f939f33de74bfc3d1d12ae5a60
Ceri Trammell <ceritrammell@Ceris-Air.lan>
2021-10-28 14:29:10 -0500

    fixed conflict

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,60 +17,14 @@ var formSubmitHandler = function(event) {
         if(response.ok) {
             console.log(response);
             response.json().then(function(data) {
-                console.log(data);
-                displayRecipeCard(data);
+                
             });
         };
     });
 };
 
-// IN THE DISPLAY CARDS A LOOP WILL BE BETTER THAN ALL THE REPEATED CODE
-// need to loop through data and display different object properties at different indexes of array returned from API
 
-var displayRecipeCard = function(data) {
-  
-    var placeHoldEl = document.getElementById("place-hold-rec");
-    var cardContentEl = document.getElementById("card-rec");
-    var cardTitleEl = document.createElement("p");
-    cardTitleEl.setAttribute("class", "title");
-    cardTitleEl.textContent = data.hits[0].recipe.label;
-    var cardSubTitleEl = document.createElement("p");
-    cardSubTitleEl.setAttribute("class", "subtitle");
-    cardSubTitleEl.textContent = data.hits[0].recipe.cuisineType;
-    cardContentEl.append(cardTitleEl, cardSubTitleEl);
-    if(placeHoldEl) {
-        placeHoldEl.remove();
-    }
 
-    // commented out below because it is still old code copied from displayBreweryCard()
-
-    // var placeHoldTwoEl = document.getElementById("place-hold-two-rec");
-    // var cardContentTwoEl = document.getElementById("card-rec-two");
-    // var cardTitleTwoEl = document.createElement("p");
-    // cardTitleTwoEl.setAttribute("class", "title");
-    // cardTitleTwoEl.textContent = data[1].name;
-    // var cardSubTitleTwoEl = document.createElement("p");
-    // cardSubTitleTwoEl.setAttribute("class", "subtitle");
-    // cardSubTitleTwoEl.textContent = data[1].street;
-    // cardContentTwoEl.append(cardTitleTwoEl, cardSubTitleTwoEl);
-    // if(placeHoldTwoEl) {
-    //     placeHoldTwoEl.remove();
-    // }
-
-    // var placeHoldThreeEl = document.getElementById("place-hold-three-rec");
-    // var cardContentThreeEl = document.getElementById("card-rec-three");
-    // var cardTitleThreeEl = document.createElement("p");
-    // cardTitleThreeEl.setAttribute("class", "title");
-    // cardTitleThreeEl.textContent = data[2].name;
-    // var cardSubTitleThreeEl = document.createElement("p");
-    // cardSubTitleThreeEl.setAttribute("class", "subtitle");
-    // cardSubTitleThreeEl.textContent = data[2].street;
-    // cardContentThreeEl.append(cardTitleThreeEl, cardSubTitleThreeEl);
-    // if(placeHoldThreeEl) {
-    //     placeHoldThreeEl.remove();
-    // }
-    
-};
 
 
 
