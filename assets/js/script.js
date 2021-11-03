8ad5105367a0203c7a6df9bfc12c27ceff512fe3
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-11-02 20:10:13 -0500

    fixed conflicts I think

--- assets/js/script.js
+++ assets/js/script.js
@@ -115,6 +115,7 @@ var generateCards = function() {
 var userFormBreweryEl = document.getElementById("user-form-brewery"); 
 var cityInputEl = document.getElementById("city-search");
 var breweryFilter = document.getElementById("brewery-type");
+var moreDrinks = document.getElementById("moreDrinks");
 
 var breweryFormSubmitHandler = function(event) {
     event.preventDefault(); 
@@ -138,7 +139,8 @@ var breweryFormSubmitHandler = function(event) {
             console.log(response);
             response.json().then(function(data) {
                 console.log(data);
-                breweryResults = data
+                moreDrinks.classList.toggle("is-hidden")
+                breweryResults = data;
                 displayBreweryCard();
             });
         };
@@ -223,5 +225,12 @@ moreRecipes.addEventListener("click", function(event) {
     console.log(foodResultsIndex, foodResultsCount);
 generateCards();
 });
+
+moreDrinks.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log(breweryResultsIndex, breweryResultsCount);
+    displayBreweryCard();
+});
+
 userFormEl.addEventListener("submit", formSubmitHandler);
 userFormBreweryEl.addEventListener("submit", breweryFormSubmitHandler);
